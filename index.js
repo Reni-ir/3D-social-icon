53661e8050f61f5c2521529d084e1d3bd4eee69f
Reni-ir <nikzad.ahmadreza@yahoo.com>
2024-10-13 22:52:29 +0330

    Create and Update

---
+++ index.js
@@ -0,0 +1,9 @@
+// function colorbox() {
+//   const parent = document.querySelectorAll(".layer");
+//   console.log(parent);
+//   parent.forEach((item) => {
+//       console.log(item);
+//       console.log(item.lastElementChild);
+//   });
+// }
+// colorbox();
