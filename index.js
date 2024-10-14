93acb4e27aa51419bdfb977d1fad6367ef1baa6f
Reni-ir <nikzad.ahmadreza@yahoo.com>
2024-10-14 10:47:28 +0330

    update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-// function colorbox() {
-//   const parent = document.querySelectorAll(".layer");
-//   console.log(parent);
-//   parent.forEach((item) => {
-//       console.log(item);
-//       console.log(item.lastElementChild);
-//   });
-// }
-// colorbox();
+const Nikzad = document.querySelector(".ahmadreza");
+const shownikzad = document.querySelectorAll(".icons a");
+function show(){
+    shownikzad.forEach((item)=>{
+        
+        item.addEventListener("mouseover", ()=>{
+            Nikzad.style="opacity:0.8;top:-100px;transition:all 0.3s;"
+        })
+        item.addEventListener("mouseout", ()=>{
+            Nikzad.style="opacity:0;top:-200px;transition:all 0.3s;"
+        })
+
+    })
+}
+show();
